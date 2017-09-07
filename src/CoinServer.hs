9762a37b59a982a07a6a1fe3836f6059285b35cc
Chris <chris@improbable.io>
2017-09-07 13:15:49 +0100

    Adding a simple example of STM shared access

--- src/CoinServer.hs
+++ src/CoinServer.hs
@@ -14,72 +14,74 @@ import Control.Exception(try,SomeException)
 import System.FilePath
 import Data.List(isPrefixOf)
 
+import Control.Monad.STM
+import GHC.Conc
+
+
 serve :: IO ()
-serve = serverWith defaultConfig { srvLog = stdLogger, srvPort = 8888 }
-     $ \_ url request ->
-  case rqMethod request of
+serve = do 
+    shared <- atomically $ newTVar (0 :: Int)
+    serverWith defaultConfig { srvLog = stdLogger, srvPort = 8888 }
+     $ \_ url request -> do
+      atomically $ readTVar shared >>= writeTVar shared . (+1)   
+      requests <- atomically $ readTVar shared
+      case rqMethod request of
 
-    GET ->
-      do
-        print (url_path url)
-        let ext = takeExtension (url_path url)
-        mb_txt <- try (readFile (url_path url))
-        case mb_txt of
-           Right a -> return $ if ext == ".html"
-                                  then sendHTML OK (primHtml a)
-                                  else sendScript OK a
-           Left (e :: SomeException) -> return $ sendHTML NotFound $
-               thehtml $ concatHtml
-                 [ thead noHtml
-                 , body $ concatHtml
-                    [ toHtml "I could not find "
-                    , toHtml $ exportURL url { url_type = HostRelative }
-                    , toHtml ", so I made this with XHTML combinators. "
-                    , toHtml $ hotlink "example.html" (toHtml "Try this instead.")
-                    ]
-                 ]
+        GET ->
+          do
+            let ext = takeExtension (url_path url)
+            mb_txt <- try (readFile (url_path url))
+            case mb_txt of
+               Right a -> return $ if ext == ".html"
+                                      then sendHTML OK (primHtml a)
+                                      else sendScript OK a
+               Left (e :: SomeException) -> return $ sendHTML NotFound $
+                   thehtml $ concatHtml
+                     [ thead noHtml
+                     , body $ concatHtml
+                        [ toHtml "In total, there have been "
+                        , toHtml $ show requests
+                        , toHtml " requests so far"
+                        ]
+                     ]
 
-                 -- where _hack :: SomeException
-                       -- _hack = e   -- to specify the type
+        POST ->
+          return $
+          case findHeader HdrContentType request of
+            Just ty
+              | "application/x-www-form-urlencoded" `isPrefixOf` ty ->
+              case URL.importParams txt of
+                Just fields -> sendHTML OK $
+                  toHtml "You posted a URL encoded form:" +++ br +++
+                  toHtml (show fields) +++ br +++
+                  hotlink "example.html" (toHtml "back")
+                Nothing -> sendHTML BadRequest $
+                  toHtml "Could not understand URL encoded form data"
 
+              | "multipart/form-data" `isPrefixOf` ty ->
+              case Form.fromRequest request of
+                Just fields -> sendHTML OK $
+                  toHtml "You posted a multipart form:" +++ br +++
+                  toHtml (show (Form.toList fields)) +++ br +++
+                  hotlink "example.html" (toHtml "back")
+                Nothing -> sendHTML BadRequest $
+                  toHtml "Could not understand multipart form data"
 
-    POST ->
-      return $
-      case findHeader HdrContentType request of
-        Just ty
-          | "application/x-www-form-urlencoded" `isPrefixOf` ty ->
-          case URL.importParams txt of
-            Just fields -> sendHTML OK $
-              toHtml "You posted a URL encoded form:" +++ br +++
-              toHtml (show fields) +++ br +++
-              hotlink "example.html" (toHtml "back")
-            Nothing -> sendHTML BadRequest $
-              toHtml "Could not understand URL encoded form data"
+              | "application/json" `isPrefixOf` ty ->
+              case runGetJSON readJSValue txt of
+                Right val -> sendJSON OK $
+                  JSObject $ toJSObject [("success", JSString $ toJSString "hello")]
+                Left err -> sendJSON BadRequest $
+                  JSObject $ toJSObject [("error", JSString $ toJSString err)]
 
-          | "multipart/form-data" `isPrefixOf` ty ->
-          case Form.fromRequest request of
-            Just fields -> sendHTML OK $
-              toHtml "You posted a multipart form:" +++ br +++
-              toHtml (show (Form.toList fields)) +++ br +++
-              hotlink "example.html" (toHtml "back")
-            Nothing -> sendHTML BadRequest $
-              toHtml "Could not understand multipart form data"
+            x -> sendHTML BadRequest $
+                 toHtml $ "I don't know how to deal with POSTed content" ++
+                          " of type " ++ show x
 
-          | "application/json" `isPrefixOf` ty ->
-          case runGetJSON readJSValue txt of
-            Right val -> sendJSON OK $
-              JSObject $ toJSObject [("success", JSString $ toJSString "hello")]
-            Left err -> sendJSON BadRequest $
-              JSObject $ toJSObject [("error", JSString $ toJSString err)]
+            -- we assume UTF8 encoding
+            where txt = decodeString (rqBody request)
 
-        x -> sendHTML BadRequest $
-             toHtml $ "I don't know how to deal with POSTed content" ++
-                      " of type " ++ show x
-
-        -- we assume UTF8 encoding
-        where txt = decodeString (rqBody request)
-
-    _ -> return $ sendHTML BadRequest $ toHtml "I don't understand"
+        _ -> return $ sendHTML BadRequest $ toHtml "I don't understand"
 
 
 
